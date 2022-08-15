8c6a794e93e5067af9ede528c46b3d764da09118
MdShohanAli <720shohan@gmail.com>
2022-08-15 12:41:30 +0600

    final push

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,29 @@
-console.log(54);
+
+
+const loadDog = dog => {
+    fetch('https://api.thedogapi.com/v1/breeds')
+        .then(res => res.json())
+        .then(data => displayDog(data))
+}
+
+const displayDog = (dogList) => {
+    // console.log(dogList)
+    const main = document.getElementById('main');
+    const firstTen = dogList.slice(0, 10);
+
+    for (const dog of firstTen) {
+        const div = document.createElement('div');
+        console.log(dog);
+        div.className = "col-lg-4"
+        div.innerHTML = `
+         <h2> ${dog.name} </h2>
+         <p>${dog.temperament}</p>
+         <img width ="400px" height ="250px" src = "${dog.image.url}"/>
+
+        `;
+        main.appendChild(div);
+
+    }
+
+
+}
